2097b5adb7a202fb2fad1a460252320fc9c7a86c
phosfox <mordredur@googlemail.com>
2020-01-18 23:52:51 +0100

    adds Expression Class and Visitor

---
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -0,0 +1,14 @@
+abstract class Expression {
+    static class Binary extends Expression {
+        Binary(Expression left, Token operator, Expression right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        final Expression left;
+        final Token operator;
+        final Expression right;
+
+    }
+}
